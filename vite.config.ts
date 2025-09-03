4262d471b3d569c4434e87a09bfbec0e89b11648
xiaoxiaojian <1837125655@qq.com>
2025-09-03 20:05:56 +0800

    修复了一些已知问题

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,14 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false, // 关闭 sourcemap
-    minify: 'terser', // 使用 terser 进行代码压缩
+    outDir: 'dist', // 指定构建输出目录
+    assetsDir: 'assets', // 指定静态资源（图片、CSS、JS 等）目录
+    sourcemap: false, // 关闭 sourcemap生成，source map 文件会暴露源代码，不适合生产环境
+    minify: 'terser', // 使用 terser 进行JS代码压缩
+    // rollup打包工具配置
     rollupOptions: {
       output: {
+        // 代码分割，让常用库单独缓存，减少用户重复下载
         manualChunks: {
           'element-plus': ['element-plus', '@element-plus/icons-vue'],
           router: ['vue-router'],
